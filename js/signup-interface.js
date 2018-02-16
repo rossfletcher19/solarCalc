f720874e4c6b7606cddb70920ef084d9083fa4e4
Ross Fletcher <rtbfletch@outlook.com>
2018-02-16 08:39:30 -0800

    remove interface and js files that were not needed

--- js/signup-interface.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function () {
-//   $('#signup').submit(function (event) {
-//     event.preventDefault();
-//     // var name = $('#name').val();
-//     // $('#signup').hide();
-//     // $('#nameOutput').prepend('<p>Thank you, ' + name + ' , now enter your age to see it in seconds!</p>');
-//   });
-// });
